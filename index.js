ef4c4cb15ab0301919065e702731cbce5468ab3b
bulbulkhan420 <79450874+bulbulkhan420@users.noreply.github.com>
2021-09-19 18:58:21 +0600

    Add files via upload

---
+++ index.js
@@ -0,0 +1,11 @@
+$(document).ready(function(){
+    var sh=0;
+    $(".fa-bars").click(function(){
+    
+       $(".slid").css("right","0px");
+    
+    })
+    $(".fa-times").click(function(){
+        $(".slid").css("right","-100%");
+    })
+})
